d758a6df8fc77cbe9de5c9177a5c13b1ba3395c5
Anton <158073528+AntonAbabii@users.noreply.github.com>
2025-06-05 10:41:40 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {/// <summary>
+     /// Задача1: Перечисление названий месяцев года.
+     /// </summary>
+        enum Month
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (Month month in Enum.GetValues(typeof(Month)))
+            {
+                Console.WriteLine($"{(int)month} - {month}");
+            }
+            Console.ReadKey();
+        }
+
+        
+        
+    }
+}
