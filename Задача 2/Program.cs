9da0794b0fc3157e9d761372f021e7f7ceb7b81b
Anton <158073528+AntonAbabii@users.noreply.github.com>
2025-06-05 10:53:20 +0300

    Add files via upload

---
+++ Задача 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        enum days 
+        {
+            sat, 
+            sun,
+            mon,
+            tue,
+            wed,
+            thu,
+            fri         
+        }
+        static void Main(string[] args)
+        {
+            foreach (days month in Enum.GetValues(typeof(days)))
+            {
+                Console.WriteLine($"{month} имеет значения  {(int)month}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
