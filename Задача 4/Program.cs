7f5e610c22fd9f39bd1a5a94fb21a84136e517a9
Anton <158073528+AntonAbabii@users.noreply.github.com>
2025-06-05 11:12:25 +0300

    Add files via upload

---
+++ Задача 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    class Program
+    { enum UserRole
+        {
+            Администратор,
+            Модератор,
+            Пользователь,
+            Гость
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите вашу роль: Администратор Модератор Пользователь Гость");
+            string input = Console.ReadLine();
+
+            bool isValid = Enum.TryParse(input, ignoreCase: true, out UserRole role);
+
+            if (isValid)
+            {
+                switch (role)
+                {
+                    case UserRole.Администратор:
+                        Console.WriteLine("Вы зарегистрированы как администратор.");
+                        break;
+                    case UserRole.Модератор:
+                        Console.WriteLine("Вы зарегистрированы как модератор.");
+                        break;
+                    case UserRole.Пользователь:
+                        Console.WriteLine("Вы зарегистрированы как пользователь.");
+                        break;
+                    case UserRole.Гость:
+                        Console.WriteLine("Вы зарегистрированы как гость.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введена некорректная роль. Допустимые значения: Admin, Moderator, User, Guest.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
